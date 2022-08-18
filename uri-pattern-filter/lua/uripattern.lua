b8792465f87d658a746fe6a06e3fee3313696526
sabarishkre <96181959+sabarishkre@users.noreply.github.com>
2022-08-18 18:58:06 +0530

    New envoyfilter and dimension, that emits URL Pattern instead of the actual URL in order to solve cardinality issues with unbounded string "URL"

---
+++ uri-pattern-filter/lua/uripattern.lua
@@ -0,0 +1,31 @@
+function getPattern(uriPath, patternlist)
+    local pathWithoutQueryParams = ""
+    for token in string.gmatch(uriPath, "[^?]+") do
+      pathWithoutQueryParams = token
+      break
+    end
+    if string.sub(pathWithoutQueryParams, -1, -1) == "/" then
+      pathWithoutQueryParams=string.sub(pathWithoutQueryParams, 1, -2)
+    end
+    local finalPath = pathWithoutQueryParams
+    for i = 1 , #(patternlist) do
+      local entry = patternlist[i]
+      local regPattern = string.gsub(entry, "%-","%%-")
+      regPattern = string.gsub(regPattern, "{.-}", "[^/]+")
+      if string.find(pathWithoutQueryParams, regPattern) then
+          finalPath = entry
+        break
+      end
+    end
+    return finalPath
+end
+function envoy_on_request(request_handle)
+  local fullPath = request_handle:headers():get(":path")
+  local pattern = getPattern(fullPath, uriPatternList)
+  request_handle:streamInfo():dynamicMetadata():set("envoy.lua.uri-pattern-matcher", "uri-pattern", pattern)
+end
+function envoy_on_response(response_handle)
+  local pattern = response_handle:streamInfo():dynamicMetadata():get("envoy.lua.uri-pattern-matcher")["uri-pattern"]
+  response_handle:headers():replace("x-internal-uri-pattern", pattern)
+end
+
